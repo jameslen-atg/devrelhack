acb8fbe96c1f5d4163ab387b3754643b74ab9e8d
Nick Landry <nicklan@microsoft.com>
2024-10-24 11:38:18 -0700

    Shooting balls badly

--- Player.py
+++ Player.py
@@ -25,6 +25,29 @@ player_rect.center = (screen_width // 2, screen_height // 2)
 player_speed = 5
 rotation_angle = 0
 
+# Load ball sprite
+ball_image = pygame.image.load(r'Assets\PNG\Default size\Ship parts\cannonBall.png')
+
+# Ball class
+class Ball:
+    def __init__(self, x, y, direction):
+        self.image = ball_image
+        self.rect = self.image.get_rect(center=(x, y))
+        self.direction = direction
+        self.speed = 10
+
+    def move(self):
+        radians = math.radians(self.direction)
+        self.rect.x += self.speed * math.cos(radians)
+        self.rect.y += self.speed * math.sin(radians)
+
+    def is_off_screen(self):
+        return (self.rect.right < 0 or self.rect.left > screen_width or
+                self.rect.bottom < 0 or self.rect.top > screen_height)
+
+# List to hold balls
+balls = []
+
 # Game loop
 running = True
 while running:
@@ -45,6 +68,20 @@ while running:
     # if keys[pygame.K_DOWN]:
     #     player_rect.y += player_speed
 
+    if keys[pygame.K_e]:
+        # Propel ball to the right of the ship
+        radians = math.radians(rotation_angle)
+        ball_x = player_rect.centerx + player_rect.width // 2 * math.cos(radians)
+        ball_y = player_rect.centery + player_rect.height // 2 * math.sin(radians)
+        balls.append(Ball(ball_x, ball_y, rotation_angle - 90))
+
+    if keys[pygame.K_q]:
+        # Propel ball to the left of the ship
+        radians = math.radians(rotation_angle)
+        ball_x = player_rect.centerx - player_rect.width // 2 * math.cos(radians)
+        ball_y = player_rect.centery - player_rect.height // 2 * math.sin(radians)
+        balls.append(Ball(ball_x, ball_y, rotation_angle + 90))
+
     # Rotate the player image
     rotated_image = pygame.transform.rotate(player_image, rotation_angle)
     rotated_rect = rotated_image.get_rect(center=player_rect.center)
@@ -54,6 +91,14 @@ while running:
 
     # Draw the player
     screen.blit(rotated_image, rotated_rect)
+
+    # Move and draw balls
+    for ball in balls[:]:
+        ball.move()
+        if ball.is_off_screen():
+            balls.remove(ball)
+        else:
+            screen.blit(ball.image, ball.rect)
 
     # Update the display
     pygame.display.flip()
