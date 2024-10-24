ef92bbc85bd2f00a3ea7dffe2f4bdb65282bfe7f
Nick Landry <nicklan@microsoft.com>
2024-10-24 10:20:16 -0700

    Changed background color and sprite for player ship

--- spaceinvaders/Player.py
+++ spaceinvaders/Player.py
@@ -10,17 +10,16 @@ screen_height = 600
 
 # Colors
 black = (0, 0, 0)
-white = (255, 255, 255)
+ocean_blue = (0, 105, 148)
 
 # Set up the display
 screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption('Simple Pygame Example')
 
-# Player settings
-player_size = 50
-player_color = white
-player_x = screen_width // 2
-player_y = screen_height // 2
+# Load player sprite
+player_image = pygame.image.load(r'Assets\PNG\Default size\Ships\ship (1).png')
+player_rect = player_image.get_rect()
+player_rect.center = (screen_width // 2, screen_height // 2)
 player_speed = 5
 
 # Game loop
@@ -32,19 +31,19 @@ while running:
 
     keys = pygame.key.get_pressed()
     if keys[pygame.K_LEFT]:
-        player_x -= player_speed
+        player_rect.x -= player_speed
     if keys[pygame.K_RIGHT]:
-        player_x += player_speed
+        player_rect.x += player_speed
     if keys[pygame.K_UP]:
-        player_y -= player_speed
+        player_rect.y -= player_speed
     if keys[pygame.K_DOWN]:
-        player_y += player_speed
+        player_rect.y += player_speed
 
-    # Fill the screen with black
-    screen.fill(black)
+    # Fill the screen with ocean blue
+    screen.fill(ocean_blue)
 
     # Draw the player
-    pygame.draw.rect(screen, player_color, (player_x, player_y, player_size, player_size))
+    screen.blit(player_image, player_rect)
 
     # Update the display
     pygame.display.flip()
