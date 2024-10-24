45cd6d9387cf1398260a059dc4b555959d36d321
amartz <amartz@microsoft.com>
2024-10-23 20:29:54 -0700

    checking in tileset and world

---
+++ IslandTileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="IslandTileset" tilewidth="128" tileheight="128" tilecount="96" columns="16">
+ <image source="Assets/Tilesheet/tiles_sheet@2.png" width="2048" height="768"/>
+</tileset>
